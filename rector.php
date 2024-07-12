8c3ee9ec2c59a75026252811b15b80f84ec432b4
Emir <emir.kurtanovic2@gmail.com>
2024-07-12 10:53:04 +0200

    set to 8.2 as it supports persistance right away

--- rector.php
+++ rector.php
@@ -13,7 +13,7 @@ return RectorConfig::configure()
         __DIR__ . '/tests',
     ])
     // uncomment to reach your current PHP version
-    ->withPhpSets(php83: true)
+    ->withPhpSets(php82: true)
     ->withRules([
         AddVoidReturnTypeWhereNoReturnRector::class,
     ]);
